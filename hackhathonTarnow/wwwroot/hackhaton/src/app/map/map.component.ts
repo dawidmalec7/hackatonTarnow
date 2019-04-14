1c0224f5cc0d9225b5ab0af257a55f69b74e18ec
Jakub Faliszewski <jfaliszewski@interia.pl>
2019-04-14 23:01:30 +0200

    some maps

--- hackhathonTarnow/wwwroot/hackhaton/src/app/map/map.component.ts
+++ hackhathonTarnow/wwwroot/hackhaton/src/app/map/map.component.ts
@@ -25,14 +25,14 @@ export class MapComponent implements OnInit {
   constructor(private definedPlaces: DefinedPlaces, private mapStyle: MapStyle) { }
 
   ngOnInit() {
-    
+
     this.getLocation().then(
       () => {
         this.initMap(this.findPlace);
-        
+
       }
     );
-           this.geocoder = new google.maps.Geocoder();
+    this.geocoder = new google.maps.Geocoder();
 
   }
 
@@ -43,18 +43,25 @@ export class MapComponent implements OnInit {
     });
   }
 
- findPlace() {
+  findPlace() {
     let t = this;
-    let address = document.getElementById('address').value;
-    this.geocoder.geocode({'address': address}, function(results, status) {
+    let address: HTMLInputElement = (<any>document.querySelector('#address')).value;
+    this.geocoder.geocode({ 'address': address }, function (results, status) {
       if (status === 'OK') {
+        let location = [results[0].geometry.location.lat(), results[0].geometry.location.lng()];
+        t.findClosestPlace(location);
         t.map.setCenter(results[0].geometry.location);
         t.map.zoom = 16;
       } else {
-        alert('Geocode was not successful for the following reason: ' + status);
+        alert('Wystąpił błąd wyszukiwania: ' + status);
       }
     });
   }
+
+  findClosestPlace(cords) {
+    console.log(cords);
+    console.log(this.closestDistance(this.definedPlaces, cords[0], cords[1]));
+  } 
 
 
   initMap(findPlace) {
@@ -66,72 +73,72 @@ export class MapComponent implements OnInit {
       url: '../../assets/car-red-min.png', size: new google.maps.Size(25, 42)
     };
 
-      t.map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: t.startLatitude, lng: t.startLongitude },
-        zoom: 13,
-        styles: t.mapStyle.style
-      });
+    t.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: t.startLatitude, lng: t.startLongitude },
+      zoom: 13,
+      styles: t.mapStyle.style
+    });
 
-      for (let i = 0; i < t.definedPlaces.placRybny.length; i++) {
-        console.log(i);
-        let pos = {
-          lat: t.definedPlaces.placRybny[i][1],
-          lng: t.definedPlaces.placRybny[i][0]
-        };
+    for (let i = 0; i < t.definedPlaces.placRybny.length; i++) {
+      console.log(i);
+      let pos = {
+        lat: t.definedPlaces.placRybny[i][1],
+        lng: t.definedPlaces.placRybny[i][0]
+      };
 
-        if (i == 2) {
-          t.markers[i] = new google.maps.Marker({
-            position: pos,
-            map: t.map,
-            title: 'zajete',
-            icon: icon_taken
-          });
-        }
-        else {
-          t.markers[i] = new google.maps.Marker({
-            position: pos,
-            map: t.map,
-            title: 'wolne',
-            icon: icon_free
-          });
-        }
+      if (i == 2) {
+        t.markers[i] = new google.maps.Marker({
+          position: pos,
+          map: t.map,
+          title: 'zajete',
+          icon: icon_taken
+        });
       }
-      var mcOptions = {gridSize: 60, maxZoom: 16, zoomOnClick: true, minimumClusterSize: 2};
-      let visibleMarkers = t.markers;
-      new MarkerClusterer(t.map, visibleMarkers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' },mcOptions)
+      else {
+        t.markers[i] = new google.maps.Marker({
+          position: pos,
+          map: t.map,
+          title: 'wolne',
+          icon: icon_free
+        });
+      }
+    }
+    var mcOptions = { gridSize: 60, maxZoom: 16, zoomOnClick: true, minimumClusterSize: 2 };
+    let visibleMarkers = t.markers;
+    new MarkerClusterer(t.map, visibleMarkers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' }, mcOptions)
 
 
     //  document.getElementById('findPlace').addEventListener('click', function() {
     //    findPlace(geocoder, this.map);
     //  });
   }
-  
-closestDistance(parkCords, geo_lat, geo_lon){
-  let distances = [];
-  parkCords.forEach(function(location, i) {
-   distances[i] = {
-      distance: this.calculateDistance(geo_lat, geo_lon, location.lat, location.lng),
-      city: location.title
-    }
-  });
-  let closestLoc = distances.reduce((prev, current) => (prev.distance < current.distance) ? prev : current);
-  
-  return closestLoc;
-}
 
- calculateDistance(lat1, lon1, lat2, lon2) {
-  if (lat1 === '' || lon1 === '' || lat2 === '' || lon2 === '') {
-    return 999999999999;
+  closestDistance(parkCords, geo_lat, geo_lon) {
+    let distances = [];
+    parkCords.forEach(function (location, i) {
+      distances[i] = {
+        distance: this.calculateDistance(geo_lat, geo_lon, location.lat, location.lng),
+        city: location.title
+      }
+    });
+    let closestLoc = distances.reduce((prev, current) => (prev.distance < current.distance) ? prev : current);
+
+    return closestLoc;
   }
 
-  var p = 0.017453292519943295;
-  var c = Math.cos;
-  var a = 0.5 - c((lat2 - lat1) * p) / 2 +
-    c(lat1 * p) * c(lat2 * p) *
-    (1 - c((lon2 - lon1) * p)) / 2;
+  calculateDistance(lat1, lon1, lat2, lon2) {
+    if (lat1 === '' || lon1 === '' || lat2 === '' || lon2 === '') {
+      return 999999999999;
+    }
 
-  return 12742 * Math.asin(Math.sqrt(a));
-}
+    var p = 0.017453292519943295;
+    var c = Math.cos;
+    var a = 0.5 - c((lat2 - lat1) * p) / 2 +
+      c(lat1 * p) * c(lat2 * p) *
+      (1 - c((lon2 - lon1) * p)) / 2;
+
+    return 12742 * Math.asin(Math.sqrt(a));
+  }
 
   filterPlaces() {
     console.log(this.map);
