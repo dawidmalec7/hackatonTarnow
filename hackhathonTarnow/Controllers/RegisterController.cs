b2d5da0c2da37943c6e56a2641d815be182ff68e
Krystian Smoleń <krystian.smolen@microsoft.wsei.edu.pl>
2019-04-14 10:07:14 +0200

    Add Register Controller

---
+++ hackhathonTarnow/Controllers/RegisterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using hackhathonTarnow.Context;
+using hackhathonTarnow.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hackhathonTarnow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private MySqlContext _context { get; set; }
+        public RegisterController(MySqlContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult<HttpResponseMessage>> UserRegister([FromBody] User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok("Rejestracja przebiegła pomyślnie");
+        }
+
+    }
+}
