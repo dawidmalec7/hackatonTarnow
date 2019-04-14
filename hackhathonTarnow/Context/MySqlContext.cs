9e50a2b4fad6adc09819f87730f8c3d6725db401
Dominik Kozioł <dkoziol@interia.com>
2019-04-14 10:25:09 +0200

    add context

---
+++ hackhathonTarnow/Context/MySqlContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hackhathonTarnow.Context
+{
+    public class MySqlContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+        public MySqlContext(DbContextOptions<MySqlContext> options) : base(options) { }
+    }
+}
