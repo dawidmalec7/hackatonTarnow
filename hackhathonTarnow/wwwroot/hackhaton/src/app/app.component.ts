52ce3b66f885233cf28bd9d392b16b3e034636d4
Jakub Faliszewski <jfaliszewski@interia.pl>
2019-04-14 12:37:28 +0200

    menu, flex main

--- hackhathonTarnow/wwwroot/hackhaton/src/app/app.component.ts
+++ hackhathonTarnow/wwwroot/hackhaton/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hackhaton';
+  menuVisible = false;
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+  }
 }
