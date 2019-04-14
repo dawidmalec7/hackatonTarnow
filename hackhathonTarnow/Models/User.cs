b29c1805e6f09ee065280e55f8dd33fecb024ec4
Dominik Kozioł <dkoziol@interia.com>
2019-04-14 11:16:14 +0200

    User model - update

--- hackhathonTarnow/Models/User.cs
+++ hackhathonTarnow/Models/User.cs
@@ -12,5 +12,14 @@ namespace hackhathonTarnow.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
+        [Range(000000000, 999999999)]
+        public int PESEL { get; set; }
+        public string CardId { get; set; }
+
     }
 }
