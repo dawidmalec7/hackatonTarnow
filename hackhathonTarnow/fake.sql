828cdc5a2084f8a42cb3773bb86c140901826443
Krystian Smoleń <krystian.smolen@microsoft.wsei.edu.pl>
2019-04-15 09:02:02 +0200

    update fake.sql

--- hackhathonTarnow/fake.sql
+++ hackhathonTarnow/fake.sql
@@ -8,67 +8,35 @@ use parkings;
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("185a99c8-d4de-4171-901e-ef2d1e417b64", false, null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e",  50.013471, 20.988950, "disabled");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("506fd533-978c-4da4-bc7e-6258b064a291", false, null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013461, 20.988991, "disabled");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("040ff480-00a1-421c-a083-2fa3f34cfeaf", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013459, 20.989032, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("d91c87c3-4443-4e2d-b455-84e3aea12868", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013460, 20.989072, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("edaa4a41-8737-4819-85c0-9ecbeb691401", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013456, 20.989115, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("8458cd2c-3b5f-4629-abd9-71fca52f58d9", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013467, 20.989145, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("17316cc1-7555-4e9e-b975-f8937c9a8458", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013458, 20.989172, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("4b526df5-6fa4-427e-ab20-febd81f5c74e", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013458, 20.989211, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("683ed233-c274-44c5-bdb7-6f20545f8938", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013461, 20.989249, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("770ae6a6-8c5e-4c09-b2b0-eadaee3bfed0", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013461, 20.989284, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("096f49ab-cc8a-4b35-9653-405bfe8d41f2", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013334, 20.988859, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("c5b1b0f4-2a69-4de7-8f44-7e0ce0c85912", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013336, 20.988910, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("b6893227-46fe-4648-95b0-721b07453003", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013339, 20.988956, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("2a2359cd-0661-4499-9dd4-cf65120bf84e", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013345, 20.989002, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("dbbf067f-16a1-4a5f-a695-37c664704754", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013350, 20.989037, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("f9ab763c-1b29-4eb6-87c8-d603df1607e3", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013353, 20.989074, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("3c4ccbb6-4a25-483a-8334-8675aedf9b48", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013355, 20.989107, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("75dfe9d5-96c7-4ff1-b9ee-f950d20159a1", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013358, 20.989144, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("c9479341-8053-4430-acba-08d0d91edaa3", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013364, 20.989181, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("9a77d4d3-cd9c-43a2-bc0b-a598c9e00245", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013372, 20.989224, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("4ce95eca-0204-4bca-8bf9-dda7d295a5c7", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013215, 20.988850, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("7ddcf072-5a2f-4a7c-9c26-71b416599ad2", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013217, 20.988887, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("b8886680-23b4-4622-b878-d1ef15c94442", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013221, 20.988914, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("b7057448-f14b-4e32-9af7-69f197a78a22", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013228, 20.988952, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("9fa18aa9-8b59-4c9b-baba-bd37a0147549", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013232, 20.988987, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("14c5c77e-8f51-4de8-85c2-409cdac27ce6", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013241, 20.989025, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("1a0e3343-fda9-4c01-8cd9-aaf456419fc3", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013245, 20.989066, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("b8afe767-5d9e-41fc-8167-607706347046", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013247, 20.989104, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("7c841820-8f98-40c2-8a7b-9fb2f9dc8b6d", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013251, 20.989141, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("93f2e458-bf0d-4be8-9876-49da9f16ef76", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013255, 20.989175, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("39a55569-dbee-4756-9de1-241e452c111e", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013260, 20.989212, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("f6d360d0-6448-4f68-a7b2-834b8d484134", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013267, 20.989240, "car");
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("1574af47-2e76-4c16-a70f-95734f23005d", false,  null, "9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", 50.013278, 20.989271, "car");
 
@@ -77,79 +45,61 @@ insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceTyp
 -- 	NumberOfFreeCyclesPlaces, Longtitude, Latitude
 -- ) values ("60890ce0-a228-4374-9bd8-d9d4ca9a822d", "Lwowska 24, 33-100 Tarnów", 38, 38, 35, 3, 0, 50.013799, 20.995933 );
 
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("f149afd4-5de4-48f0-9e5c-67fe0b47dc99", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013766, 20.995999 , "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("ee3f2e8e-c17b-46f3-9497-82f59a5a0eaa", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013788, 20.995988, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("ddce453f-24b7-48c7-b6b3-188c90004e8e", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013811, 20.995983, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("5a19d492-0e42-4d95-b018-14eb7104ef95", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013834, 20.995981, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("591dbb69-0655-4884-a605-b0dfd5dceae1", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013853, 20.995973, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("096193be-2e37-476d-aaf8-0b8accc91627", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013875, 20.995969, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("581ab137-c2da-413d-a236-9817df795c24", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013900, 20.995961, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("d4f0a4c5-6785-4ff3-912b-be9491d9c1b8", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013921, 20.995953, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("23cc06c4-2122-4b7e-a086-4809036d764e", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013944, 20.995943, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("1285025e-ba95-4a41-9b58-53d6062140ea", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013969, 20.995941, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("168ee562-6211-4fd8-a337-bad95c302168", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.013992, 20.995932, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("acbc0afa-13da-4753-a5a9-c480b093e41b", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014013, 20.995920, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("bda7d1d6-5f22-4038-9cd7-28b19aeda880", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014032, 20.995913, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("08efd8bc-1d61-4fa6-84fc-b74f457b3ed4", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014055, 20.995902, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("b9a8ad4c-c2e5-41b2-ad85-f7e9474b10fc", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014078, 20.995892, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("9de37e94-d913-439a-ad01-b2fff036e8a8", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014100, 20.995883, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("6ae3f9de-a1c2-4253-af42-1206c913ffa5", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014122, 20.995872, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("69165c3b-91ee-4d73-9ac1-4819ca9a82ec", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014145, 20.995865, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("81ae6d90-4607-454e-a4d2-9d51e1dff47e", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014168, 20.995862, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("52269461-3b11-407b-914a-7c3fa8705895", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014189, 20.995847, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("08b941a5-3587-4bc9-b108-c1c8f9f993ab", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014237, 20.995826, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude)
 	values ("0138c3bf-e23d-40aa-86ca-75ffe8eab65f", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014255, 20.995812);
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("87ead69c-f8ef-414a-8a20-5c98dcb13356", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014277, 20.995796, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("d00969de-e03f-4ee7-971c-4a79b9634321", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014304, 20.995778, "disabled");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("648930ea-b7e8-4c5d-9cf0-17a2175acba3", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014333, 20.995770, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("74d63471-0804-439b-b739-c92ec0945a3c", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014352, 20.995751, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("af614dfc-5bea-4a3e-ba23-fa9db2a19514", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014376, 20.995737, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("9afeb4b4-7f02-4ad0-8d87-848ac29610d0", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014398, 20.995728, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("18881ce3-606f-4336-8805-506187348cde", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014420, 20.995720, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("e40d924b-80bd-4058-ba4f-b4f3f61e59b3", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014444, 20.995709, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("da1c5a3e-fe66-48c9-babb-3d7276146574", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014470, 20.995702, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("5424af55-22b1-4dde-b7cc-b8c4dda94a60", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014494, 20.995696, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("f3e53f3d-d955-4923-a39f-a83987e594ea", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014518, 20.995692, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("8e408300-0c01-4367-928b-60cedf4d90f6", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014545, 20.995683, "car");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("9fd99cc1-17bc-44ad-a8af-d96e8bace8cd", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014570, 20.995684, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
 	values ("34b4d494-736b-42c9-b01d-22791da14822", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014596, 20.995679, "car");
+
 insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("83c0c65e-ba10-4d1e-b451-2787a0498fee", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014620, 20.995684, "disabled");
-insert into spaces (id, IsBusy, Plate, ParkingId, Longtitude, Latitude, SpaceType)
-	values ("11f78669-996b-44b0-8712-1cb073045123", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014646, 20.995676, "disabled");insert into parkings (  id, Address, NumberOfPlaces, NumberOfFreePlaces, NumberOfFreeCarsPlaces, NumberOfFreeDisabledPlaces,   NumberOfFreeCyclesPlaces, Longtitude, Latitude ) values ("9b1dbbc2-0ff4-4189-91d1-cc3b5defab9e", "Rybna 33-110 Tarnów", 29, 29, 25, 2, 2, 50.013379, 20.988927 )
+	values ("11f78669-996b-44b0-8712-1cb073045123", false,  null, "60890ce0-a228-4374-9bd8-d9d4ca9a822d", 50.014646, 20.995676, "disabled");
+
